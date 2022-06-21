5fa9922a89a657a63804459312db092c37e53611
nguyennhuy1704 <khanhandli@gmail.com>
2022-06-21 19:28:29 +0700

    Config: Config absolute path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import routes from '../config/router';
-import EmployeePage from '../features/employee/page';
-import HomePage from '../features/home/page';
+import EmployeePage from '~features/employee/page';
+import HomePage from '~features/home/page';
 
 // Public routes
 const publicRoutes: [] = [];
